fe7bbb37d5deadcd38fc02c200730427c0ec3782
Marcel Dzubba <mail@marcel-dzubba.de>
2022-07-07 22:27:52 +0200

    singularity for umin = 0

--- kran_3.jl
+++ kran_3.jl
@@ -6,35 +6,33 @@ g   = 9.81
 v_soll = 0.4
 v_min = -10.0
 a_max  = 0.33
-a_min = -a_max#0.0
+a_min = 0.0
 phi_0 = 0
 dphi_0 = 0
 phi_tf = 0
 dphi_tf = 0
 
-N = 2000
+N = 3*30
 
 using JuMP, Ipopt, PyCall, PyPlot, DelimitedFiles
 mod = Model(optimizer_with_attributes(Ipopt.Optimizer, "max_iter" => 1000, "mumps_mem_percent" => 8000))
-@variable(mod, tf >= 0.1, start = 10.0)              # Optimierungshorizont [0, tf]
+@variable(mod, tf >= 0.1, start = 1.5918761117048705)              # Optimierungshorizont [0, tf]
 @variable(mod, a_min/K_x <= u[0:N-1] <= a_max/K_x, start = 0)
-@variable(mod, -10.0 <= x1[0:N] <= 10.0, start = 0)
+@variable(mod, x1[0:N])
 @variable(mod, x2[0:N])
 @variable(mod, x3[0:N])
-
-@constraint(mod, u[0] == 0)
+#@constraint(mod, u[0] == 0)
 @constraint(mod, x1[0] == 0)
 @constraint(mod, x2[0] == phi_0)
 @constraint(mod, x3[0] == dphi_0)
 @constraint(mod, x1[N] == v_soll)
 @constraint(mod, x2[N] == phi_tf)
 @constraint(mod, x3[N] == dphi_tf)
+@NLconstraint(mod, x1_collo[i=0:N-1],x1[i+1] - x1[i] == tf / N * u[i])
+@NLconstraint(mod, x2_collo[i=0:N-1],x2[i+1] - x2[i] == tf / N * (x3[i]+x3[i+1])/2.0)
+@NLconstraint(mod, x3_collo[i=0:N-1],x3[i+1] - x3[i] == tf / N * (- u[i]-g*(x2[i]+x2[i+1])/2.0)/l_s)
 
-for i in 0:N-1
-    @NLconstraint(mod, x1[i+1] - x1[i] == tf / N * u[i])
-    @NLconstraint(mod, x2[i+1] - x2[i] == tf / N * (x3[i]+x3[i+1])/2.0)
-    @NLconstraint(mod, x3[i+1] - x3[i] == tf / N * (- u[i]-g*(x2[i]+x2[i+1])/2.0)/l_s)
-end
+
 
 @objective(mod, Min, tf)
 
@@ -53,7 +51,7 @@ if termination_status(mod) == JuMP.MathOptInterface.LOCALLY_SOLVED
     x3_sol = value.(x3.data)
     u_sol  = value.(u.data)
     for i in 1:N-1
-        if abs(u_sol[i]-u_sol[i+1]) > 0.1
+        if abs(u_sol[i]-u_sol[i+1]) > 0.2
             t_i = t[i]
             println(t_i)
         end
@@ -79,4 +77,14 @@ if termination_status(mod) == JuMP.MathOptInterface.LOCALLY_SOLVED
     plot(t,x3_sol)
     xlabel("time")
     ylabel("dphi")
+    if a_min == 0
+        writedlm("kran_a_min_0.dat", [collect(0:N)/N*value(tf) [value.(u.data); NaN] value.(x1.data) value.(x2.data) value.(x3.data)])
+        writedlm("kran_a_min_0_dual.dat", [(collect(0:N-1).+0.5)/N*value(tf) dual.(x1_collo.data) dual.(x2_collo.data) dual.(x3_collo.data) ])
+
+    else
+        writedlm("kran.dat", [collect(0:N)/N*value(tf) [value.(u.data); NaN] value.(x1.data) value.(x2.data) value.(x3.data)])
+        writedlm("kran_dual.dat", [(collect(0:N-1).+0.5)/N*value(tf) dual.(x1_collo.data) dual.(x2_collo.data) dual.(x3_collo.data) ])
+
+    end
+
 end
--- kran_5.m
+++ kran_5.m
@@ -1,9 +1,8 @@
 function kran_5()
+close all
 global x0 xf umax umin g
 
-t1= 0.697;
-t2= .893;
-tf= 1.592;
+scenario = 1;
 %# Modellparameter
 K_x = 0.99;
 l_s = 1.0;
@@ -17,27 +16,98 @@ dphi_0 = 0;
 phi_tf = 0;
 dphi_tf = 0;
 u_max = a_max/K_x;
-u_min = eps(10);
 umax = u_max;
-umin = -u_max;
-N = 200;
-x0 = [0 0 0]';
+switch scenario
+    case 0
+        t1= 0.6898200664133287;
+        t2= 0.8888066240325581;
+        tf= 1.5918924609538354;
+        umin = -u_max;
+        results_X = load('kran.dat');
+        N1 = (size(results_X,1)-1)/3;
+        x10 = 0;%results_X(1,3);
+        x20 = 0;%results_X(1,4);
+        x30 = 0;%results_X(1,5);
+        results_dual = load('kran_dual.dat');
+        p10 = results_dual(1,2);
+        p20 = results_dual(1,3);
+        p30 = results_dual(1,4);        
+    case 1
+        t1= 0.5879036776475854;
+        t2= 0.9798394627459757;
+        tf= 1.6033736663115965;
+        umin = 0.00000;
+        results_X = load('kran_a_min_0.dat');
+        N1 = (size(results_X,1)-1)/3;
+        x10 = results_X(1,3);
+        x20 = results_X(1,4);
+        x30 = results_X(1,5);
+        results_dual = load('kran_a_min_0_dual.dat');
+        p10 = results_dual(1,2);
+        p20 = results_dual(1,3);
+        p30 = results_dual(1,4);
+end
+% N1 = 100;
+x0 = [x10 x20 x30]';
 xf = [v_soll, 0,0]';
-solinit = bvpinit([linspace(0, 1.0, 20) linspace(1.0, 2.0, 20), ...
-    linspace(2.0, 3.0, 20)],[0 0 0 0 0 0], [t1; t2;tf]);
-sol = bvp4c(@ode, @bc, solinit, bvpset('stats', 'on', 'RelTol', 1e-8));
+% solinit = bvpinit([linspace(0, 1.0, N1) linspace(1.0, 2.0, N1), ...
+%     linspace(2.0, 3.0, N1)],[0 0 0 0 0 0], [t1; t2;tf]);
+solinit = bvpinit([linspace(0, 1.0, N1) linspace(1.0, 2.0, N1), ...
+     linspace(2.0, 3.0, N1)],[x10;x20;x30; p10; p20; p30], [t1; t2;tf]);
+sol = bvp4c(@ode, @bc, solinit, bvpset('stats', 'on', 'RelTol', 1e-12, ...
+    'AbsTol', 1e-10));
 t1 = sol.parameters(1);
 t2 = sol.parameters(2);
 tf = sol.parameters(3);
-t = [linspace(0, 1-eps(1), 50) linspace(1+eps(1), 2-eps(1), 50)...
-     linspace(2+eps(1), 3, 50)]';
+N2 = 300;
+t = [linspace(0, 1-eps(1), N2) linspace(1+eps(1), 2-eps(1), N2)...
+     linspace(2+eps(1), 3, N2)]';
 y = deval(sol, t);
 xs = deval(sol, 1-eps(1)); % Knick
+u = zeros(length(t),1);
+for i = 1:length(u)
+    if t(i) < 1
+        u(i) = umax;
+    elseif t(i) < 2
+        u(i) = umin;
+    else
+        u(i) = umax;
+    end
+end
 t = [t(t<1.0)*t1; t1+(t(t>1.0& t <= 2.0)-1.0)*(t2-t1);...
      t2+(t(t>2.0)-2.0)*(tf-t2)]; % Entnormierung
 % H = 1+p(:,1).*(-0.5*x(:, 1)+x(:,2))+p(:,2).*(-x(:, 2)+u);
 x = y(1:3, :)';    
 p = y(4:6, :)';
+H = 1+p(:,1).*u+p(:,2).*x(:,3)-p(:,3).*(x(:,2)*g+u)/l_s;
+figure(1)
+tiledlayout(3,2)
+ax1 = nexttile;
+plot(t, x(:,1))
+ax2 = nexttile;
+plot(t, x(:,2))
+ax3 = nexttile;
+plot(t, x(:,3))
+ax4 = nexttile;
+plot(t, p(:,1))
+hold on
+plot(results_dual(:,1), results_dual(:,2))
+ax5 = nexttile;
+plot(t, p(:,2))
+hold on
+plot(results_dual(:,1), results_dual(:,3))
+ax6 = nexttile;
+plot(t, p(:,3))
+linkaxes([ax1, ax2, ax3,ax4,ax5,ax6], 'x')
+hold on
+plot(results_dual(:,1), results_dual(:,4))
+figure(2)
+tiledlayout(2,1)
+bx1 = nexttile;
+plot(t,u);
+bx2 =nexttile;
+plot(t,H);
+linkaxes([bx1, bx2],'x')
 
     function yd = ode(t, y, int_nr, par)
         % kanonisches Dgl.-System
@@ -64,17 +134,17 @@ p = y(4:6, :)';
         x_0 = y_0(1:3, 1);              % x(0)
         x_f = y_f(1:3, 3);              % x(tf)
         p_f = y_f(4:6, 3);              % p(tf)
-        p_s2 = y_f(4:6, 2);              % p(tf)        
-        p_s1 = y_f(4:6, 1);              % p(t1)
+        p_s2 = y_f(4:6, 2);             % p(t2)        
+        p_s1 = y_f(4:6, 1);             % p(t1)
         u_f = umax;                     % u(tf)
         H_f = 1+p_f(1)*u_f + p_f(2)*x_f(3) -p_f(3)*(g*x_f(3) + u_f)/l_s; % H(tf)
         res = [x_0-x0; ...              % x(0)  = x0
             x_f-xf; ...                 % x(tf) = xf
             H_f; ...                    % H(tf) = 0
             y_f(:, 1)-y_0(:, 2); ...    % Stetigkeit x, p in ts
-            y_f(:, 2)-y_0(:, 3); ...
-            p_s1(1)-p_s1(3)/l_s; ...
-            p_s2(1)-p_s2(3)/l_s];                    % Schaltbedingung
+            y_f(:, 2)-y_0(:, 3); ...    % -""-
+            p_s1(1)-p_s1(3)/l_s; ...    % Schaltbedingung
+            p_s2(1)-p_s2(3)/l_s];       % -""-            
     end
 
 
